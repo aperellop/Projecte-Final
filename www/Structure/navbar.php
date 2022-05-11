8d980d40f74f9c9d600bb997b73d599733180840
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-11 17:06:07 +0200

    .

--- www/Structure/navbar.php
+++ www/Structure/navbar.php
@@ -1,29 +1,27 @@
 <?php
-
-function navbar(){
-    print('
-        <nav class="navbar navbar-default container p-5 pb-0 mb-0">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <a title="Logo" href="index.php">   <img src="./../Resources/Photographies/Structure/logo.jpg" alt="Logo" width="100" height="100" /> </a>
-                                    </div>
-                                    <div class="col-8 p-5 text-center">
-                                        <p style="line-height: 50%" class="h5 text-center text-light">the</p>
-                                        <p style="line-height: 50%" class="h1 text-center text-light">Crack</p>
+    function navbar(){
+        print('
+            <nav class="navbar navbar-default container p-5 pb-0 mb-0">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <a title="Logo" href="index.php">   <img src="./../Resources/Photographies/Structure/logo.jpg" alt="Logo" width="100" height="100" /> </a>
+                                        </div>
+                                        <div class="col-8 p-5 text-center">
+                                            <p style="line-height: 50%" class="h5 text-center text-light">the</p>
+                                            <p style="line-height: 50%" class="h1 text-center text-light">Crack</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav> 
-    ');
-}
-
+            </nav> 
+        ');
+    }
 ?>
