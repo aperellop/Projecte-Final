4bdee05f7f043f977bb7fdc1c91d6e1bf775b401
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-11 17:02:26 +0200

    navbar

--- www/Structure/footer.php
+++ www/Structure/footer.php
@@ -1,17 +1,15 @@
 <?php
-
-function footer(){
-    print('
-            <footer class="footer">
-                <div class="container p-5 pb-0 mb-0">
-                    <div class="row">
-                            <p class="text-muted">Albert Perelló Puertas</p>
-                            <p class="text-muted">Web Applications Implementation</p>
-                            <p class="text-muted">2nd Dual ASIX</p>
+    function footer(){
+        print('
+                <footer class="footer">
+                    <div class="container p-5 pb-0 mb-0">
+                        <div class="row">
+                                <p class="text-muted">Albert Perelló Puertas</p>
+                                <p class="text-muted">Web Applications Implementation</p>
+                                <p class="text-muted">2nd Dual ASIX</p>
+                        </div>
                     </div>
-                </div>
-            </footer> 
-        ');
-}
-
+                </footer> 
+            ');
+    }
 ?>
