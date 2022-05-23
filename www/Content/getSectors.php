06d14feb1a1dc822f94f4d41a264c2bf04e68dc2
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-23 17:44:45 +0200

    funciones rutas y sectores

---
+++ www/Content/getSectors.php
@@ -0,0 +1,31 @@
+<?php
+    include './Structure/navbar.php';
+    include 'classSpot.php';
+    include 'connection.php';
+?>
+
+<?php
+    navbar();
+?>
+
+<?php
+    function getSpots(){
+        // Conexión a la base de datos
+        $db = connectDB();
+
+        $result = $db->stmt_init();
+        $result->prepare('SELECT * FROM Spots;');
+        $result->execute();
+        $result->bind_result($name, $description, $approach, $conditions, $photography);
+
+        while ($result->fetch() != null){ //Recorremos los registros devueltos
+            
+            //Creamos el array de objetos
+            $spot[] = new Spot ($name, $description, $approach, $conditions, $photography);
+        }
+        return $spot;
+        
+        $result->close();
+        $db->close();
+    }
+?>
