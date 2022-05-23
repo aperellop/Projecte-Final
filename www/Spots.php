50145a71c93dc467e40a7e7ae97fa7d86ccd1b57
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-23 17:21:33 +0200

    info spot

--- www/Spots.php
+++ www/Spots.php
@@ -42,27 +42,51 @@
             $i = $_GET["id"];
             $spots = getSpots($i);
 
-            print_r($spots);
+            // print_r($spots);
 
             print('
-                <div class="card d-flex mx-lg-5 text-center">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-5">
-                                <h3 class="card-title">'.$Spots->name.'</h3>
-                                <p class="card-text">'.$Spots->description.'</p>
-                                <p class="card-text">'.$Spots->approach.'</p>
-                                <p class="card-text">'.$Spots->conditions.'</p>
-                            </div>
+                    </br>
+                    </br>
+                    <div class="card d-flex mx-lg-5">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5 sm-5">
+                                    <a href="/Spots.php?id='.$i.'"><p class="h3 card-title text-center">'.$spots[$i]->name.'</p></a>
+                                    <p class="card-text">'.$spots[$i]->description.'</p>
+                                    <p class="h4">Aproximación<p>
+                                    <p class="card-text">'.$spots[$i]->approach.'</p>
+                                    <p class="h4">Condiciones<p>
+                                    <p class="card-text">'.$spots[$i]->conditions.'</p>
+                                </div>
 
-                            <div class="col-sm-5">
-                                <img src="'.$Spots->photography.'" width="200">
+                                <div class="col-5 sm-5">
+                                    <img src="'.$spots[$i]->photography.'" width="300">
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                </br>
-            '); 
+                    </br>
+                ');
+
+            // print('
+            //     <div class="card d-flex mx-lg-5 text-center">
+            //         <div class="card-body">
+            //             <div class="row">
+            //                 <div class="col-sm-5">
+            //                     <h3 class="card-title">'.$Spots->name.'</h3>
+            //                     <p class="card-text">'.$Spots->description.'</p>
+            //                     <p class="card-text">'.$Spots->approach.'</p>
+            //                     <p class="card-text">'.$Spots->conditions.'</p>
+            //                 </div>
+
+            //                 <div class="col-sm-5">
+            //                     <img src="'.$Spots->photography.'" width="200">
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     </br>
+            // '); 
         ?>
 
         <?php
