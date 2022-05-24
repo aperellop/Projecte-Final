1055a3596d6ca9d41b4f483c84557f174fedf68b
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-24 18:57:22 +0200

    estructura

--- www/Structure/session.php
+++ www/Structure/session.php
@@ -12,7 +12,7 @@
                         <p class="mx-lg-5 text-light" >'.$_SESSION['email'].'</p>
                     </div>
                     <div class="col-1">
-                        <a class="mx-lg-5" href="logout.php"><button type="button">Log out</button></a>
+                        <a class="mx-lg-5" href="./../logout.php"><button type="button">Log out</button></a>
                     </div>
                 </div>
             </div>
@@ -21,7 +21,7 @@
             // Sinó printa un saludo genérico y el botón para logearse
             print('
                 <p class="h5 mx-lg-5 text-light" >Hello, guest</p>
-                <a class="mx-lg-5" href="login.php"><button type="button">Log in</button></a>
+                <a class="mx-lg-5" href="./../login.php"><button type="button">Log in</button></a>
             ');
         }
     }
