b51ed4038897829ea468c2481a0bc1d51241c01c
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-24 18:53:22 +0200

    sesion

--- www/Spots.php
+++ www/Spots.php
@@ -41,6 +41,7 @@
             include './Content/getSpots.php';
             include './Content/getSectors.php';
             include './Structure/navbar.php';
+            include './Structure/session.php';
             include './Structure/footer.php';
         ?>
     </head>
@@ -53,35 +54,12 @@
     <body class="bg-dark margenes">
             <!-- Sesion -->
         <?php
-            // Si hay una sesión iniciada
-            if (isset($_SESSION['username'])){
-                // Printa la foto, nombre y email del usuario de la sesión y el botón para deslogearse
-                print('
-                <div class="container">
-                    <div class="row">
-                        <div class="col p-5 text-left">
-                            <img class="circular--square" src="'.$_SESSION['photography'].'" height="50" width="50">
-                            <p class="h4 mx-lg-5 text-light" >Hello, '.$_SESSION['name'].'</p>
-                            <p class="mx-lg-5 text-light" >'.$_SESSION['email'].'</p>
-                        </div>
-                        <div class="col-1">
-                            <a class="mx-lg-5" href="logout.php"><button type="button">Log out</button></a>
-                        </div>
-                    </div>
-                </div>
-                ');
-            } else {
-                // Sinó printa un saludo genérico y el botón para logearse
-                print('
-                    <p class="h5 mx-lg-5 text-light" >Hello, guest</p>
-                    <a class="mx-lg-5" href="login.php"><button type="button">Log in</button></a>
-                ');
-            }
+            session();
         ?>
             <!-- Información del Spot seleccionado -->
         <?php
-            $i = $_GET["id"];
-            $spots = getSpots($i);
+            $id = $_GET["id"];
+            $spots = getSpots();
 
             // print_r($spots);
 
@@ -105,8 +83,11 @@
         ?>
             <!-- Tarjetas de los Sectores -->
         <?php
+            $nomspot = $spots[$i]->name;
+            print($nomspot);
+
             // Introducimos la funcion getSectors en la variable $sectors
-            $sectors = getSectors();
+            $sectors = getSectors($nomspot);
         
             // Contamos cuantos objetos tenemos dentro del array $sectors y lo metemos en la variable $numSectors
             $numSectors = count($sectors);
