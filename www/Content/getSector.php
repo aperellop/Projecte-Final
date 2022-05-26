21f615e121236f289ece9b541b4e260e16389bb5
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-26 17:45:53 +0200

    get Sector

---
+++ www/Content/getSector.php
@@ -0,0 +1,25 @@
+<?php
+    include 'classSector.php';
+    // include 'connection.php';
+?>
+
+<?php
+    function getSector(){
+        // Conexión a la base de datos
+        $db = connectDB();
+
+        $result = $db->stmt_init();
+        $result->prepare('SELECT * FROM Sectors WHERE id="'.$id.'";');
+        $result->execute();
+        $result->bind_result($id, $spot, $name, $description, $approach);
+
+        while ($result->fetch() != null){ //Recorremos los registros devueltos
+            //Creamos el array de objetos
+            $sector[] = new Sector ($id, $spot, $name, $description, $approach);
+        }
+        return $sector;
+        
+        $result->close();
+        $db->close();
+    }
+?>
