d9049a0706632673c8ca30579ea1ab793d90e646
AlbertPerello <aperellop@cifpfbmoll.eu>
2022-05-27 17:07:20 +0200

    .

--- www/Structure/footer.php
+++ www/Structure/footer.php
@@ -4,8 +4,8 @@
                 <footer class="footer">
                     <div class="container p-5 pb-0 mb-0">
                         <div class="row">
-                                <p class="text-muted">Albert Perelló Puertas</p>
-                                <p class="text-muted">Administració de Sistemes Informàtics en Xarxa</p>
+                            <p class="text-muted">Administració de Sistemes Informàtics en Xarxa</p>
+                            <p class="text-muted">Albert Perelló Puertas</p>
                         </div>
                     </div>
                 </footer> 
